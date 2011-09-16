a94176c3557ae3b45437b89bc32b9b16413bc3d9
Andrew Murray <sckoobs@gmail.com>
2011-09-16 17:02:09 +0100

    Added code for the about button such that clicking it triggers the AboutActivity

--- src/com/amudev/sudoku/SudokuActivity.java
+++ src/com/amudev/sudoku/SudokuActivity.java
@@ -3,13 +3,34 @@ package com.amudev.sudoku;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-
-public class SudokuActivity extends Activity {
+public class SudokuActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // set up the click listeners for each of the buttons
+        View continueButton = findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
+    }
+    
+    public void onClick(View v) {
+    	switch (v.getId()) {
+    	case R.id.about_button:
+    		Intent i = new Intent(this, AboutActivity.class);
+    		startActivity(i);
+    		break;
+    	}
     }
 }
